1903eab94a98bec8627e4a6d4c813a3ce30e512a
Prabhu K <prabhu.k@icloud.com>
2023-11-26 23:58:41 +0530

    test api basic functionality

--- src/dhan.py
+++ src/dhan.py
@@ -1,7 +1,18 @@
 import logging
+import os
 
+from dhanhq import dhanhq
 from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
+
+logging.info("welcome to dhan api")
+
+client_id = os.environ.get("CLIENT_ID")
+access_token = os.environ.get("ACCESS_TOKEN")
+dhan = dhanhq(client_id, access_token)
+
+fund_limits = dhan.get_fund_limits()
+logging.info(fund_limits)
