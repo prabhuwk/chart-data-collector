a0e79b36f37eeb50b473e98dc3f52aaf712a6265
Prabhu K <prabhu.k@icloud.com>
2023-11-27 00:49:26 +0530

    place order

--- src/dhan.py
+++ src/dhan.py
@@ -14,5 +14,32 @@ client_id = os.environ.get("CLIENT_ID")
 access_token = os.environ.get("ACCESS_TOKEN")
 dhan = dhanhq(client_id, access_token)
 
-fund_limits = dhan.get_fund_limits()
-logging.info(fund_limits)
+# place order
+place_order = dhan.place_order(
+    tag="",
+    transaction_type=dhan.SELL,
+    exchange_segment=dhan.NSE,
+    product_type=dhan.INTRA,
+    order_type=dhan.MARKET,
+    validity="DAY",
+    security_id="58275",
+    quantity=1,
+    disclosed_quantity=0,
+    price=0,
+    trigger_price=0,
+    after_market_order=False,
+    amo_time="OPEN",
+    bo_profit_value=0,
+    bo_stop_loss_Value=0,
+    drv_expiry_date=None,
+    drv_options_type="CALL",
+    drv_strike_price=None,
+)
+
+if place_order["status"] == "failure":
+    logging.error(f"Failed to place order {place_order['remarks']['message']}")
+if place_order["status"] == "success":
+    logging.info(
+        f"Order placed.\norder_id {place_order['data']['orderId']}"
+        f"order status is {place_order['data']['orderStatus']}"
+    )
