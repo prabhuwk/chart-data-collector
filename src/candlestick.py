8721d4fca4e1e403118f67375b4d3c8d4eaa8cfa
Prabhu K <prabhu.k@icloud.com>
2023-11-30 04:17:05 +0530

    Update candlestick.py

--- src/candlestick.py
+++ src/candlestick.py
@@ -1,7 +1,7 @@
 import logging
 import os
+from datetime import datetime, timedelta
 
-import numpy as np
 import pandas as pd
 import pandas_ta as ta
 from dhanhq import dhanhq
@@ -16,16 +16,41 @@ client_id = os.environ.get("CLIENT_ID")
 access_token = os.environ.get("ACCESS_TOKEN")
 dhan = dhanhq(client_id, access_token)
 
-# minute_chart = dhan.historical_minute_charts(
-#     symbol="BANKNIFTY",
-#     exchange_segment="IDX_I",
-#     instrument_type="INDEX",
-#     expiry_code=0,
-#     from_date="2023-11-26",
-#     to_date="2023-11-27",
-# )
-# logging.info(minute_chart)
+# get yesterday data for calculating cpr
+yesterday = datetime.today() - timedelta(days=1)
+yesterday_date = yesterday.date().strftime("%Y-%m-%d")
+today_date = datetime.today().date().strftime("%Y-%m-%d")
+yesterday_minute_chart = dhan.historical_minute_charts(
+    symbol="BANKNIFTY",
+    exchange_segment="IDX_I",
+    instrument_type="INDEX",
+    expiry_code=0,
+    from_date=yesterday_date,
+    to_date=today_date,
+)
 
+yesterday_df = pd.DataFrame(data=yesterday_minute_chart["data"])
+
+temp_list = []
+for i in yesterday_df["start_Time"]:
+    temp = dhan.convert_to_date_time(i)
+    temp_list.append(temp)
+yesterday_df["converted_date_time"] = temp_list
+yesterday_df.set_index(
+    pd.DatetimeIndex(yesterday_df["converted_date_time"]), inplace=True
+)
+
+# Function to calculate CPR
+pivot = (yesterday_df["high"] + yesterday_df["low"] + yesterday_df["close"]) / 3
+bc = (yesterday_df["high"] + yesterday_df["low"]) / 2
+tc = (pivot * 2) - bc
+
+# Extract the last values (yesterday's CPR)
+pivot_value = pivot.iloc[-1]
+bc_value = bc.iloc[-1]
+tc_value = tc.iloc[-1]
+
+# get intraday minute chart
 minute_chart = dhan.intraday_daily_minute_charts(
     security_id="25", exchange_segment="IDX_I", instrument_type="INDEX"
 )
@@ -36,9 +61,9 @@ for i in df["start_Time"]:
     temp = dhan.convert_to_date_time(i)
     temp_list.append(temp)
 df["converted_date_time"] = temp_list
-df.index = pd.to_datetime(df.converted_date_time)
+df.set_index(pd.DatetimeIndex(df["converted_date_time"]), inplace=True)
 df_5min = df.resample("5T").agg(
-    {"open": "first", "high": "max", "low": "min", "close": "last"}
+    {"open": "first", "high": "max", "low": "min", "close": "last", "volume": "sum"}
 )
 
 # Calculating the 20 EMA
@@ -47,61 +72,21 @@ df_5min["20_ema"] = ta.ema(df["close"], length=20)
 # Create an additional plot for the 20 EMA
 ema_plot = make_addplot(df_5min["20_ema"], color="yellow", width=1.0)
 
-
-# Function to calculate CPR
-def calculate_cpr(df):
-    df["PP"] = (df["high"].shift(1) + df["low"].shift(1) + df["close"].shift(1)) / 3
-    df["TC"] = (df["high"].shift(1) + df["low"].shift(1)) / 2
-    df["BC"] = (df["TC"] * 2) - df["PP"]
-    # df["R1"] = (2 * df["PP"]) - df["low"].shift(1)
-    # df["S1"] = (2 * df["PP"]) - df["high"].shift(1)
-    # df["R2"] = df["PP"] + (df["high"].shift(1) - df["low"].shift(1))
-    # df["S2"] = df["PP"] - (df["high"].shift(1) - df["low"].shift(1))
-    return df
-
-
-# Applying CPR calculation
-df_5min = calculate_cpr(df_5min)
+# Assign the CPR values to each row in the intraday data
+df_5min["Pivot"] = pivot_value
+df_5min["BC"] = bc_value
+df_5min["TC"] = tc_value
 
 # Create an additional plot for CPR
-pp_plot = make_addplot(df_5min["PP"], color="gray", width=1.0)
-tc_plot = make_addplot(df_5min["TC"], color="orange", width=1.0)
-bc_plot = make_addplot(df_5min["BC"], color="orange", width=1.0)
+pp_plot = make_addplot(df_5min["Pivot"], color="blue", width=1.0)
+tc_plot = make_addplot(df_5min["TC"], color="red", width=1.0)
+bc_plot = make_addplot(df_5min["BC"], color="green", width=1.0)
 
-
-# Identifying the crossover points for buy and sell Signals
-cross_above = (df_5min["20_ema"] > df_5min["TC"]) & (
-    df_5min["20_ema"].shift(1) <= df_5min["TC"].shift(1)
-)
-cross_below = (df_5min["20_ema"] < df_5min["BC"]) & (
-    df_5min["20_ema"].shift(1) >= df_5min["BC"].shift(1)
-)
-
-
-df_5min["buy"] = np.where(cross_above, df_5min["close"], np.nan)
-df_5min["sell"] = np.where(cross_below, df_5min["close"], np.nan)
-
-
-buy_signal_plot = make_addplot(
-    df_5min["buy"],
-    type="scatter",
-    markersize=200,
-    marker="^",
-    color="green",
-)
-sell_signal_plot = make_addplot(
-    df_5min["sell"],
-    type="scatter",
-    markersize=200,
-    marker="v",
-    color="red",
-)
-
-# Creating a candlestick chart with mplfinance
+# Create candlestick chart with mplfinance
 plot(
     df_5min,
     type="candle",
-    addplot=[ema_plot, pp_plot, tc_plot, bc_plot, buy_signal_plot, sell_signal_plot],
+    addplot=[ema_plot, pp_plot, tc_plot, bc_plot],
     style="charles",
     volume=False,
     title="5-Minute Intraday Candlestick Chart",
