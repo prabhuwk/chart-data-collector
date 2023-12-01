fffda059ed0748001880f8685e962863f90c7313
Prabhu K <prabhu.k@icloud.com>
2023-12-01 14:53:31 +0530

    download trade symbols file

--- src/main.py
+++ src/main.py
@@ -1,17 +1,26 @@
 import logging
+from pathlib import Path
 
 import click
 from process import process_data
-from utils import get_dhan_client
+from utils import download_file, get_dhan_client
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 @click.command()
+@click.option(
+    "--trade-symbols-file",
+    type=click.Path(),
+    default="downloads/api-scrip-master.csv",
+    help="trade symbols file",
+)
 @click.option("--symbol-name", required=True, help="name of symbol")
 @click.option("--exchange", required=True, help="name of exchange")
-def main(symbol_name: str, exchange: str):
+def main(trade_symbols_file: str, symbol_name: str, exchange: str):
+    if not Path(trade_symbols_file).exists():
+        download_file(trade_symbols_file)
     dhan_client = get_dhan_client()
     process_data(dhan_client, symbol_name, exchange)
 
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,33 @@
+import logging
 import os
+from pathlib import Path
 
+import requests
 from dhanhq import dhanhq
 from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+TRADE_SYMBOLS_FILE_URL = "https://images.dhan.co/api-data/api-scrip-master.csv"
+
+
+class DownloadFailedError(Exception):
+    """Failed to download file"""
+
+
+def download_file(trade_symbols_file) -> None:
+    download_directory = Path(trade_symbols_file).parent
+    Path(download_directory).mkdir(parents=True, exist_ok=True)
+    response = requests.get(TRADE_SYMBOLS_FILE_URL, stream=True)
+    if response.status_code == 200:
+        with open(trade_symbols_file, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        logger.info(f"downloaded file {trade_symbols_file} successfully.")
+        return
+    message = f"failed to download file {trade_symbols_file}"
+    raise DownloadFailedError(message)
 
 
 def get_dhan_client():
