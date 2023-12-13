7513ed582d9f34f7dd8521ebb51d42ed0d5be3ba
Prabhu K <prabhu.k@icloud.com>
2023-12-13 23:37:22 +0530

    Update signals.py

--- src/signals.py
+++ src/signals.py
@@ -1,7 +1,7 @@
 from pandas.core.frame import DataFrame
 
 
-def find_support_resistance(levels: list, df: DataFrame):
+def support_resistance_green_candle(levels: list, df: DataFrame):
     for i in range(len(levels) - 1):
         support = df[levels[i]]
         resistance = df[levels[i + 1]]
@@ -10,9 +10,18 @@ def find_support_resistance(levels: list, df: DataFrame):
     return False, False
 
 
+def resistance_support_red_candle(levels: list, df: DataFrame):
+    for i in range(len(levels) - 1):
+        resistance = df[levels[i]]
+        support = df[levels[i + 1]]
+        if support < df["close"] < resistance:
+            return resistance, support
+    return False, False
+
+
 def calculate_buy_signal(df: DataFrame) -> bool:
     levels = ["s4", "s3", "s2", "s1", "bc", "tc", "r1", "r2", "r3", "r4"]
-    support, resistance = find_support_resistance(levels, df)
+    support, resistance = support_resistance_green_candle(levels, df)
     if support == df["bc"] and resistance == df["tc"]:
         return False
     if not (support and resistance):
@@ -39,12 +48,12 @@ def generate_buy_signal(df: DataFrame) -> DataFrame:
 
 def calculate_sell_signal(df: DataFrame) -> bool:
     levels = ["r4", "r3", "r2", "r1", "tc", "bc", "s1", "s2", "s3", "s4"]
-    support, resistance = find_support_resistance(levels, df)
-    if support == df["tc"] and resistance == df["bc"]:
+    resistance, support = resistance_support_red_candle(levels, df)
+    if support == df["bc"] and resistance == df["tc"]:
         return False
     if not (support and resistance):
         return False
-    if df["open"] < df["s4"]:
+    if df["open"] > df["r4"]:
         return False
     treshold = (resistance - support) * 0.10
     red_candle = df["open"] > df["close"]
@@ -52,9 +61,9 @@ def calculate_sell_signal(df: DataFrame) -> bool:
     body_50_percent = (df["open"] + df["high"]) / 2
     below_ema = body_50_percent < df["20_ema"]
 
-    body_70_percent_below = body_70_percent < support
+    body_70_percent_below = body_70_percent < resistance
 
-    near_close = -treshold < abs(df["close"] - support) < treshold
+    near_close = -treshold < abs(df["close"] - resistance) < treshold
 
     return red_candle and below_ema and near_close and body_70_percent_below
 
